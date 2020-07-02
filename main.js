0118f6d0c37fc082faba491b99e7917e08f007c7
Abel Masila <abelmasila@gmail.com>
2020-07-02 18:16:11 +0300

    vue components

--- main.js
+++ main.js
@@ -1,25 +1,63 @@
-let app = new Vue({
-  el: "#app",
-  data: {
-    product: "Socks",
-    selectedVariant: 0,
-    details: ["80% cotton", "20% polyester", "Gender-neutral"],
-    brand: "Nike",
-    variants: [
-      {
-        variantId: 2234,
-        variantColor: "green",
-        variantImage: "./sock.jpg",
-        variantQty: 10,
-      },
-      {
-        variantId: 2235,
-        variantColor: "blue",
-        variantImage: "./blue.jpg",
-        variantQty: 0,
-      },
-    ],
-    cart: 0,
+Vue.component("product", {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  template: `<div class="product">
+    <div class="product-image">
+      <img :src="image" />
+    </div>
+    <div class="product-info">
+      <h1>{{title}}</h1>
+      <p v-if="inStock">In Stock</p>
+      <p v-else>Out of Stock</p>
+      <p>Shipping: {{shipping}}</p>
+      <ul>
+        <li v-for="detail in details">{{detail}}</li>
+      </ul>
+      <div
+        v-for="(variant,index) in variants"
+        :key="variant.variantId"
+        class="color-box"
+        :style="{backgroundColor:variant.variantColor}"
+        @mouseover="updateProduct(index)"
+      ></div>
+      <button
+        v-on:click="addToCart"
+        :disabled="!inStock"
+        :class="{disabledButton:!inStock}"
+      >
+        Add to Cart
+      </button>
+      <div class="cart">
+        <p>Cart ({{cart}})</p>
+      </div>
+    </div>
+  </div>`,
+  data() {
+    return {
+      product: "Socks",
+      selectedVariant: 0,
+      details: ["80% cotton", "20% polyester", "Gender-neutral"],
+      brand: "Nike",
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: "green",
+          variantImage: "./sock.jpg",
+          variantQty: 10,
+        },
+        {
+          variantId: 2235,
+          variantColor: "blue",
+          variantImage: "./blue.jpg",
+          variantQty: 0,
+        },
+      ],
+      cart: 0,
+    };
   },
   methods: {
     addToCart() {
@@ -39,5 +77,17 @@ let app = new Vue({
     inStock() {
       return this.variants[this.selectedVariant].variantQty;
     },
+    shipping() {
+      if (this.premium) {
+        return "Free";
+      }
+      return 2.99;
+    },
+  },
+});
+let app = new Vue({
+  el: "#app",
+  data: {
+    premium: false,
   },
 });
